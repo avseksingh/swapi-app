cdf626e5dcc96e28000b902f589d22380ad425d4
Abhishek Singh <avsekjob@gmail.com>
2022-08-20 13:49:06 +0530

    link added

--- capital/capitalapp/urls.py
+++ capital/capitalapp/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('', views.swapi, name='swapi'),
     path('films/', views.films, name='films'),
     path('people/', views.people, name='people'),
+    path('starships/', views.starships, name='starships'),
+    path('vehicles/', views.vehicles, name='vehicles'),
+    path('planets/', views.planets, name='planets'),
+    path('species/', views.species, name='species'),
+    # path('character/', views.character, name='character'),
 ]
--- capital/capitalapp/views.py
+++ capital/capitalapp/views.py
@@ -18,21 +18,64 @@ def swapi(request):
 
 def films(request):
     path = request.GET["path"]
-    print(path, "")
+    # print(path, "")
     url = requests.get(path)
     data = json.loads(url.text)
     # print(len(bookselflink))
-    return render(request, "selflink.html", {"data": data})
+    return render(request, "films.html", {"data": data})
     # return HttpResponse(json.dumps(data),content_type='application/json')
-    # return render(request, "selflink.html")
-
+    # return render(request, "films.html")
 
 def people(request):
     path = request.GET["path"]
-    print(path, "")
+    # print(path, "")
     url = requests.get(path)
     data = json.loads(url.text)
     # print(len(bookselflink))
-    # return render(request, "selflink.html", {"data": data})
-    return HttpResponse(json.dumps(data), content_type='application/json')
-    # return render(request,"home.html")
+    return render(request, "swapi.html", {"data": data})
+    # return HttpResponse(json.dumps(data), content_type='application/json')
+
+def vehicles(request):
+    path = request.GET["path"]
+    # print(path, "")
+    url = requests.get(path)
+    data = json.loads(url.text)
+    # print(len(bookselflink))
+    return render(request, "vehicles.html", {"data": data})
+    # return HttpResponse(json.dumps(data), content_type='application/json')
+
+# def character(request):
+#     path = request.GET["path"]
+#     print(path, "")
+#     url = requests.get(path)
+#     data = json.loads(url.text)
+#     # print(len(bookselflink))
+#     # return render(request, "films.html", {"data": data})
+#     return HttpResponse(json.dumps(data), content_type='application/json')
+
+def starships(request):
+    path = request.GET["path"]
+    # print(path, "")
+    url = requests.get(path)
+    data = json.loads(url.text)
+    # print(len(bookselflink))
+    return render(request, "starships.html", {"data": data})
+    # return HttpResponse(json.dumps(data), content_type='application/json')
+
+def planets(request):
+    path = request.GET["path"]
+    # print(path, "")
+    url = requests.get(path)
+    data = json.loads(url.text)
+    # print(len(bookselflink))
+    return render(request, "planets.html", {"data": data})
+    # return HttpResponse(json.dumps(data), content_type='application/json')
+
+def species(request):
+    path = request.GET["path"]
+    # print(path, "")
+    url = requests.get(path)
+    data = json.loads(url.text)
+    # print(len(bookselflink))
+    return render(request, "species.html", {"data": data})
+    # return HttpResponse(json.dumps(data), content_type='application/json')
